0918aa44300ea58b439d02162fe49cd3fbdb9a82
Sungjin Kang <ujuc@ujuc.kr>
2018-08-15 12:45:10 +0900

    Set url and add test view
    * 테스트용 indev view 페이지 추가
    * url 패이지 추가

--- jangbu/views.py
+++ jangbu/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {'data': 'jangbu'}
+
+        return render(request, self.template_name, context=context)
